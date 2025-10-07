846f7f456528843ee01a7f47f8b154e2332085a3
seniority1 <quicktaskteam2025@gmail.com>
2025-10-07 13:26:43 +0100

    Delete commands/commands directory

--- commands/commands/about.js
+++
@@ -1,8 +0,0 @@
-export default {
-  name: "about",
-  async execute(sock, msg) {
-    await sock.sendMessage(msg.key.remoteJid, {
-      text: "🤖 *NoxOra v1.0*\nCreated by Seniority Team 💡\nPowered by Baileys ⚙️",
-    });
-  },
-};
