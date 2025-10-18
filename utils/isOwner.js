9eeb76d53d1ccfb6800c15a1c8fe7497de5f1d2c
seniority1 <quicktaskteam2025@gmail.com>
2025-10-18 11:25:12 -1100

    Update isOwner.js

--- utils/isOwner.js
+++ utils/isOwner.js
@@ -1,5 +1,11 @@
 import config from "../config.js";
 
 export function isOwner(sender) {
-  return config.owners.some(num => sender.includes(num));
+  if (!sender) return false;
+
+  // Normalize: "2349160291884@s.whatsapp.net" → "2349160291884"
+  const cleanSender = sender.split("@")[0];
+
+  // Normalize owner list too
+  return config.owners.some(owner => cleanSender === owner.split("@")[0]);
 }
