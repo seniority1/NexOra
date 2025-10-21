665eac739f947d63ddb7caccef02b092350a233c
seniority1 <quicktaskteam2025@gmail.com>
2025-10-21 17:37:37 +0100

    Update tictactoe.js

--- utils/tictactoe.js
+++ utils/tictactoe.js
@@ -1,60 +1,50 @@
 class TicTacToe {
-  constructor(playerX = 'x', playerO = 'o') {
+  constructor(playerX, playerO) {
     this.playerX = playerX;
     this.playerO = playerO;
-    this._currentTurn = false;
     this._x = 0;
     this._o = 0;
-    this.turns = 0;
+    this._turn = false; // false = X, true = O
   }
 
   get board() { return this._x | this._o; }
-  get currentTurn() { return this._currentTurn ? this.playerO : this.playerX; }
-  get enemyTurn() { return this._currentTurn ? this.playerX : this.playerO; }
+  get currentTurn() { return this._turn ? this.playerO : this.playerX; }
 
   static check(state) {
-    for (let combo of [7,56,73,84,146,273,292,448])
+    for (const combo of [7,56,73,84,146,273,292,448])
       if ((state & combo) === combo) return true;
     return false;
   }
 
-  static toBinary(x=0,y=0) {
-    if (x<0||x>2||y<0||y>2) throw new Error('invalid position');
-    return 1 << x + (3*y);
+  static toBinary(x, y) {
+    if (x < 0 || x > 2 || y < 0 || y > 2) throw new Error("invalid pos");
+    return 1 << (x + 3 * y);
   }
 
-  turn(player=0,x=0,y) {
-    if (this.board === 511) return -3;
-    let pos = 0;
-    if (y==null) {
-      if (x<0||x>8) return -1;
-      pos = 1 << x;
-    } else {
-      if (x<0||x>2||y<0||y>2) return -1;
-      pos = TicTacToe.toBinary(x,y);
-    }
-    if (this._currentTurn ^ player) return -2;
-    if (this.board & pos) return 0;
-    this[this._currentTurn ? '_o' : '_x'] |= pos;
-    this._currentTurn = !this._currentTurn;
-    this.turns++;
+  turn(playerIndex, pos) {
+    if (this.board === 511) return -3; // full board
+    if (this._turn ^ playerIndex) return -2; // not your turn
+    if (this.board & (1 << pos)) return 0; // occupied
+
+    this[this._turn ? "_o" : "_x"] |= (1 << pos);
+    this._turn = !this._turn;
     return 1;
   }
 
-  static render(boardX=0, boardO=0) {
-    let x = parseInt(boardX.toString(2), 4);
-    let y = parseInt(boardO.toString(2), 4) * 2;
-    return [...(x + y).toString(4).padStart(9,'0')]
+  render() {
+    const map = (v) => (v === 1 ? "X" : v === 2 ? "O" : " ");
+    const x = parseInt(this._x.toString(2), 4);
+    const o = parseInt(this._o.toString(2), 4) * 2;
+    const cells = [...(x + o).toString(4).padStart(9, "0")]
       .reverse()
-      .map((value,index)=>value==1?'X':value==2?'O':++index);
+      .map(v => map(+v));
+    return cells;
   }
 
-  render() { return TicTacToe.render(this._x,this._o); }
-
   get winner() {
-    let x = TicTacToe.check(this._x);
-    let o = TicTacToe.check(this._o);
-    return x ? this.playerX : o ? this.playerO : false;
+    if (TicTacToe.check(this._x)) return this.playerX;
+    if (TicTacToe.check(this._o)) return this.playerO;
+    return false;
   }
 }
 
